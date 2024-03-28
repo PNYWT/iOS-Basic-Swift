95cb1c14fbed5470394a8c1f4dfdc5a9de88924a
Punyawat.S <99156196+PNYWT@users.noreply.github.com>
2024-03-28 16:14:05 +0700

    update

---
+++ Basic RxSwift and Alamofire/Basic RxSwift and Alamofire/UserModel.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  Basic RxSwift and Alamofire
+//
+//  Created by Dev on 28/3/2567 BE.
+//
+
+import Foundation
+
+struct UserModel: Codable {
+    let id: Int
+    let name: String
+    let email: String
+}
